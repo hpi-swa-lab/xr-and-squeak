d802e3c7b1ed9245f97e6436326d774e3851fb86
leogeier <contact@leonardgeier.com>
2024-02-01 19:17:18 +0100

    load class methods and load simple THREE scene

--- sandblocks/xr-project/main.js
+++ sandblocks/xr-project/main.js
@@ -1,5 +1,6 @@
 import { SqueakProject, ensureSystemChangeCallback, sqCompile } from "../squeak-project/main.js";
 import { button, h } from "../../view/widgets.js";
+import { useState } from "../../external/preact-hooks.mjs";
 
 export class XRProject extends SqueakProject {
   static deserialize(options) {
@@ -29,6 +30,17 @@ export class XRProject extends SqueakProject {
     await super.open();
     await ensureSystemChangeCallback(true);
     await this.updateFromRemote();
+    window.THREE = await import(
+      "https://unpkg.com/three@0.160.0/build/three.module.js"
+    );
+    // TODO: find nicer way to access external objects like this from within squeak (instead of importing like this and using `JS VRButton`)
+    window.VRButton = (
+      await import(
+        "https://unpkg.com/three@0.160.0/examples/jsm/webxr/VRButton.js"
+      )
+    ).VRButton;
+
+    sqEval('XRWorld start');
   }
 
   async updateFromRemote() {
@@ -40,16 +52,27 @@ export class XRProject extends SqueakProject {
     }
     
     for (const cls of source) {
-      for (const method of cls.methods) {
+      for (const method of cls.instanceMethods) {
         sqCompile(cls.name, method);
+      }
+
+      const metaclass = cls.name + " class";
+      for (const method of cls.classMethods) {
+        sqCompile(metaclass, method);
       }
     }
   }
 
   renderBackground() {
+    const [isLoading, setIsLoading] = useState(false);
     return h("Fragment", {}, [
       super.renderBackground(),
-      button("Update SqueakXR", () => this.updateFromRemote()),
+      button("Update SqueakXR", async () => {
+        setIsLoading(true);
+        await this.updateFromRemote();
+        setIsLoading(false);
+      }),
+      isLoading ? h("p", null, "Updating SqueakXR") : null,
     ]);
   }
 }
